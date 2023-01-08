6557686deb298e82f23ab9875661b86559b6c5f7
xIsm4 <minelatinsoporte@gmail.com>
2023-01-08 22:30:54 +0100

    Show only 1.8 version on #getBukkitVersion

--- PaperSpigot-Server/src/main/java/org/bukkit/craftbukkit/util/Versioning.java
+++ PaperSpigot-Server/src/main/java/org/bukkit/craftbukkit/util/Versioning.java
@@ -5,25 +5,11 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 
 public final class Versioning {
     public static String getBukkitVersion() {
-        String result = "Unknown-Version";
-
-        InputStream stream = Bukkit.class.getClassLoader().getResourceAsStream("META-INF/maven/org.github.paperspigot/paperspigot-api/pom.properties");
-        Properties properties = new Properties();
-
-        if (stream != null) {
-            try {
-                properties.load(stream);
-
-                result = properties.getProperty("version");
-            } catch (IOException ex) {
-                Logger.getLogger(Versioning.class.getName()).log(Level.SEVERE, "Could not get Bukkit version!", ex);
-            }
-        }
-
-        return result;
+        return "1.8.8-R0.1";
     }
 }
